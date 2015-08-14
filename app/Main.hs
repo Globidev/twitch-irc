353edff825c8fae40844088b71508cbe08000658
Rémi Vion <vion.remi@gmail.com>
2015-08-15 01:04:45 +0200

    nicer args code

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,8 @@ module Main (main) where
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.Process
-
 import Control.Monad (when)
+
 import qualified Twitch
 
 -- Next step is to use Twitch API to fetch this
@@ -14,11 +14,10 @@ pass = "oauth:c6gnjdi9tmqysf4edozkim16l77605"
 main :: IO ()
 main = do
   args <- getArgs
-  when (length args >= 2) $ do
-    let channel = (args !! 0)
-    let script = (args !! 1)
-    run channel script
-
+  case args of
+    channel:script:_ -> run channel script
+    _ -> error "specify channel and script. example: twitch-app lirik twitch-hello-world"
+  
 run :: String -> String -> IO ()
 run channel scriptFile = do
   proc <- createProcess (proc scriptFile []){std_in = CreatePipe, std_out = Inherit}
