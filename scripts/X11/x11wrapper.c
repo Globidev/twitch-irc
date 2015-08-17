71bca57b83728d5b1292bb2241e4dcb8424a835e
Globicodeur <guillaume.depardon@outlook.com>
2015-08-17 12:48:58 +0200

    [Scripts] Added a script for sending input to X11 windows

---
+++ scripts/X11/x11wrapper.c
@@ -0,0 +1,24 @@
+#include <X11/Xlib.h>
+
+Status sendInput(Display * display, Window win, Window root, int pressed, int keycode) {
+  XKeyEvent event;
+
+  event.display     = display;
+  event.window      = win;
+  event.root        = root;
+  event.subwindow   = None;
+  event.time        = CurrentTime;
+  event.x           = 1;
+  event.y           = 1;
+  event.x_root      = 1;
+  event.y_root      = 1;
+  event.same_screen = True;
+  event.keycode     = XKeysymToKeycode(display, keycode);
+  event.state       = 0; // modifiers
+
+  event.type        = (pressed ? KeyPress : KeyRelease);
+  Status press      = XSendEvent(display, win, True, KeyPressMask, (XEvent*)&event);
+  XFlush(display);
+
+  return press;
+}
